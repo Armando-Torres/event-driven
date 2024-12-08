78c31a7bafc5276fc283df0abbf269d22ce467b3
Armando Torres <armando.tinez@icloud.com>
2024-12-08 19:33:48 +0100

    Add delete line and add line use cases for an order

---
+++ producer/src/main/java/com/eventdriven/producer/Order/Application/AddOrderLines.java
@@ -0,0 +1,35 @@
+package com.eventdriven.producer.Order.Application;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.eventdriven.producer.Order.Application.Service.OrderResponse;
+import com.eventdriven.producer.Order.Domain.Order;
+import com.eventdriven.producer.Order.Domain.OrderRepository;
+import com.eventdriven.producer.Order.Domain.ValueObject.LineItem;
+import com.eventdriven.producer.Order.Domain.ValueObject.Status;
+
+@Component
+public class AddOrderLines {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public OrderResponse invoke(Long orderId, List<LineItem> linesItems) {
+        Order order = this.orderRepository.findById(orderId);
+
+        this.checkIfOrderIsOpen(order);
+
+        linesItems.stream().forEach(line -> this.orderRepository.addLine(order.getId(), line));
+
+        return new OrderResponse(order);
+    }
+
+    private void checkIfOrderIsOpen(Order order) {
+        if (order.getStatus() != Status.OPEN) {
+            throw new IllegalStateException(String.format("The order with id {%d} is CLOSED", order.getId()));
+        }
+    }
+}
---
+++ producer/src/main/java/com/eventdriven/producer/Order/Application/DeleteOrderLine.java
@@ -0,0 +1,22 @@
+package com.eventdriven.producer.Order.Application;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.eventdriven.producer.Order.Application.Service.OrderResponse;
+import com.eventdriven.producer.Order.Domain.Order;
+import com.eventdriven.producer.Order.Domain.OrderRepository;
+
+@Component
+public class DeleteOrderLine {
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public OrderResponse invoke(Long orderId, Integer listItemPosition) {
+        Order order = this.orderRepository.findById(orderId);
+
+        this.orderRepository.deleteLine(order.getId(), listItemPosition);
+
+        return new OrderResponse(order);
+    }
+}
