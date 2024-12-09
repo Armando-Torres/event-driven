5cecb1050f60e3fe9a7654b58a60b8cff91c90c2
Armando Torres <armando.tinez@icloud.com>
2024-12-09 21:56:26 +0100

    Add business logic to set closed status to order use case for publish event when it is closed

--- producer/src/main/java/com/eventdriven/producer/Order/Application/SetCloseStatusToOrder.java
+++ producer/src/main/java/com/eventdriven/producer/Order/Application/SetCloseStatusToOrder.java
@@ -7,12 +7,19 @@ import com.eventdriven.producer.Order.Application.Service.OrderResponse;
 import com.eventdriven.producer.Order.Domain.Order;
 import com.eventdriven.producer.Order.Domain.OrderRepository;
 import com.eventdriven.producer.Order.Domain.ValueObject.Status;
+import com.eventdriven.producer.Shared.Domain.MessageProducer;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
 public class SetCloseStatusToOrder {
-    
+    private final String topicName = "customer-order";
+
     @Autowired
     private OrderRepository orderRepository;
+
+    @Autowired
+    private MessageProducer messageProducer;
 
     public OrderResponse invoke(Long orderId) {        
         Order order = this.orderRepository.findById(orderId);
@@ -22,13 +29,25 @@ public class SetCloseStatusToOrder {
         order.setStatus(Status.CLOSED);
 
         Order newOrder = this.orderRepository.save(order);
+
+        this.sendOrderCloseEvent(order);
         
         return new OrderResponse(newOrder);
     }
 
     private void checkIfOrderIsClosed(Order order) {
-        if (order.getStatus() != Status.CLOSED) {
-            throw new IllegalStateException(String.format("The order with id {%d} is CLOSED", order.getId()));
+        if (order.getStatus() == Status.CLOSED) {
+            throw new IllegalStateException(String.format("The order with id %d is CLOSED", order.getId()));
         }
+    }
+
+    private void sendOrderCloseEvent(Order order) {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            this.messageProducer.sendMessage(this.topicName, order.getId().toString(), objectMapper.writeValueAsString(order));
+        } catch (JsonProcessingException e) {
+            
+        }        
     }
 }
